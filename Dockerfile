01af27ad25d7aa413d41a5da4eb6dae6077b2434
Dominiek Ter Heide <info@dominiek.com>
2018-12-14 15:23:28 -0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM ubuntu:trusty
+
+RUN apt-get -y update --fix-missing
+RUN apt-get -y install curl bash cron
+
+RUN mkdir /workdir
+WORKDIR /workdir
+COPY install_gcloud_sdk.sh /workdir
+RUN ./install_gcloud_sdk.sh
+
+ADD . /workdir
+
+CMD ["./entrypoint.sh"]
---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SOURCE_GS_BUCKET=`cat /workdir/SOURCE_GS_BUCKET.env`
+DESTINATION_GS_BUCKET=`cat /workdir/DESTINATION_GS_BUCKET.env`
+DATE_STR=`date +%y%m%d-%H%M`
+
+gsutil cp gs://$SOURCE_GS_BUCKET gs://$DESTINATION_GS_BUCKET
+echo $DATE_STR > /workdir/backup.date
+if [ ! $? -eq 0 ]; then
+    echo "Error copying file to Google Storage!!"
+    exit 1
+fi
+gsutil cp /workdir/backup.date gs://$DESTINATION_GS_BUCKET
